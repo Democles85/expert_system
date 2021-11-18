08247c081ec690204c127534445bf49c50c18a38
Damocles <democleschannel28@gmail.com>
2021-11-18 18:50:03 +0100

    Commiting Medocles V2.0

--- Medocles.java
+++ Medocles.java
@@ -4,56 +4,65 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Medocles {
-    static void most_Common_Symptoms() {
-        System.out.println("\t Fever?");
-        System.out.println("\t Cough?");
-        System.out.println("\t Tiredness?");
-        System.out.println("\t Loss of Taste or Smell?");
-        System.out.println("\t All of them?");
+    // static void flu_Symptoms() {
+    // System.out.println("\t Fever?");
+    // System.out.println("\t Cough?");
+    // System.out.println("\t Sore throat?");
+    // System.out.println("\t Runny nose?");
+    // System.out.println("\t Stuffy nose?");
+    // System.out.println("\t Muscle ache?");
+    // System.out.println("\t Body ache?");
+    // System.out.println("\t Headache?");
+    // System.out.println("\t Fatigue?");
+    // System.out.println("\t Vomiting?");
+    // System.out.println("\t Diarrhoea?");
+    // System.out.println("\t All of them?");
+    // System.out.print("Shkruani nje nga simptomat e mesiperme: ");
+    // }
+
+    // Covid Symptoms
+    static void covid_Symptoms() {
+        System.out.println("  Fever?");
+        System.out.println("  Cough?");
+        System.out.println("  Tiredness?");
+        System.out.println("  Loss of Taste or Smell?");
+        System.out.println("  Sore throat?");
+        System.out.println("  Headache?");
+        System.out.println("  Aches and pains?");
+        System.out.println("  Diarrhoea?");
+        System.out.println("  Discolouration of fingers?");
+        System.out.println("  Red or irritated eyes?");
+        System.out.println("  All of them?");
         System.out.print("Shkruani nje nga simptomat e mesiperme: ");
     }
 
-    static void all_Symptoms() {
-        System.out.println("\t Fever?");
-        System.out.println("\t Cough?");
-        System.out.println("\t Tiredness?");
-        System.out.println("\t Loss of Taste or Smell?");
-        System.out.println("\t Sore throat?");
-        System.out.println("\t Headache?");
-        System.out.println("\t Aches and pains?");
-        System.out.println("\t Diarrhoea?");
-        System.out.println("\t Discolouration of fingers?");
-        System.out.println("\t Red or irritated eyes?");
-        System.out.println("\t All of them?");
-        System.out.print("Shkruani nje nga simptomat e mesiperme: ");
-    }
-
+    // Main Function
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String answer0, answer1, answer2;
         int count = 0;
         ArrayList<String> answers = new ArrayList<String>();
 
         System.out.println("\n\t  Medocles");
         System.out.println("Cfare simptomash po shfaqni?");
-        most_Common_Symptoms();
+        covid_Symptoms();
         // Pergjigja per ciklin e pare te pyetjeve
-        String answer0;
         answer0 = input.nextLine();
         answer0 = answer0.toLowerCase();
         // ----------------------------------------------------------------------------------------------------
 
         // Knowledge base arrays
-        String[] most_Common_Symptoms = { "fever", "cough", "tiredness", "loss of taste or smell", "loss of taste",
-                "loss of smell" };
-        String[] less_Common_Symptoms = { "sore throat", "headache", "aches and pains", "diarrhoea",
-                "discolouration of fingers", "red or irritated eyes" };
-        String[] serious_Symptoms = { "difficulty breathing", "loss of speech", "chest pain" };
+        String[] covid_Symptoms = { "fever", "cough", "tiredness", "loss of taste or smell", "loss of taste",
+                "loss of smell", "sore throat", "headache", "aches and pains", "diarrhoea", "discolouration of fingers",
+                "red or irritated eyes", "red eyes", "irritated eyes", "difficulty breathing", "loss of speech",
+                "chest pain" };
+        // String[] flu_Symptoms = { "fever", "cough", "sore throat", "runny nose",
+        // "stuffy nose", "muscle ache",
+        // "body ache", "headache", "fatigue", "vomiting", "diarrhoea" };
         // ----------------------------------------------------------------------------------------------------
 
         // Kthimi ne lista i knowledge base
-        List<String> list0 = Arrays.asList(most_Common_Symptoms);
-        List<String> list1 = Arrays.asList(less_Common_Symptoms);
-        List<String> list2 = Arrays.asList(serious_Symptoms);
+        List<String> list0 = Arrays.asList(covid_Symptoms);
         // ----------------------------------------------------------------------------------------------------
 
         // Kontrolli nese pergjigjia gjendet ne knowledge base
@@ -65,27 +74,28 @@ public class Medocles {
                 System.out.println("||=> Ju shfaqni " + count + " nga simptomat me te zakonshme per Covid-19."
                         + "\n\tAto jane: " + answers);
             } else if (answer0.equals("all of them") || answer0.equals("all of them.")) {
+                System.out.println(list0);
+                count = list0.size();
                 System.out.println("||=> Ju shfaqni te gjitha simptomat me te zakonshme per Covid-19.");
+                break;
             } else {
                 System.out.println("||=> Simptoma e pershkruar nuk ekziston.");
                 System.exit(0);
             }
             // Vazhdimi i pytjeve nese shpreh simptoma te tjera
             do {
-                String answer1, answer2;
-
                 System.out.println("Keni simptoma te tjera (po ose jo)? ");
 
                 answer1 = input.nextLine();
                 answer1 = answer1.toLowerCase();
 
                 if (answer1.equals("po")) {
-                    all_Symptoms();
+                    covid_Symptoms();
 
                     answer2 = input.nextLine();
                     answer2 = answer2.toLowerCase();
 
-                    if (list0.contains(answer2) || list1.contains(answer2) && answer2 != answer0) {
+                    if (list0.contains(answer2) && answer2 != answer0) {
                         count++;
                         answers.add(answer2);
                         System.out.println("||=> Ju shfaqni " + count + " nga simptomat me te zakonshme per Covid-19."
@@ -103,11 +113,11 @@ public class Medocles {
             // ----------------------------------------------------------------------------------------------------
         } while (!list0.contains(answer0));
         // ----------------------------------------------------------------------------------------------------
-        if (answers.size() >= 4) {
-            System.out.println("Ju keni 4 ose me shume simptoma per Covid-19!");
-        } else {
-            System.out.println("Ju keni me pak se 4 simptoma te Covid-19 e si rrjedhoje mos u shqetesoni!");
-        }
+
+        float shanci = 1;
+        float length = list0.size();
+        shanci = (count / length) * 100;
+        System.out.println(shanci + "%"); // Perqindja e te pasurit Covid-19 sipas simptomave
         input.close();
     }
 }
---
+++ Medocles_v2.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Medocles_v2 {
+    static void covid_Symptoms() {
+        System.out.println("  Fever?");
+        System.out.println("  Cough?");
+        System.out.println("  Tiredness?");
+        System.out.println("  Loss of Taste or Smell?");
+        System.out.println("  Sore throat?");
+        System.out.println("  Headache?");
+        System.out.println("  Aches and pains?");
+        System.out.println("  Diarrhoea?");
+        System.out.println("  Discolouration of fingers?");
+        System.out.println("  Red or irritated eyes?");
+        System.out.println("  All of them?");
+    }
+
+    static void yes_answer(List<String> list, String before_answer) {
+        Scanner input = new Scanner(System.in);
+
+        float chance = 1;
+        float size = list.size();
+
+        ArrayList<String> answers = new ArrayList<String>();
+        answers.add(before_answer);
+        // System.out.println(answers);
+        String answer;
+        int n;
+
+        System.out.print("How many of the symptomes above are you showing? ");
+        n = input.nextInt();
+        System.out.println("Enter the symptoms that you show below.");
+        for (int i = 0; i <= n; i++) {
+            answer = input.nextLine();
+            answer = answer.toLowerCase();
+            answers.add(answer);
+        }
+        System.out.println(answers);
+        chance = ((n + 1) / size) * 100;
+        System.out.println("The chances of you containing Covid-19 based on the symptoms is: " + chance + "%");
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String[] covid_Symptoms = { "fever", "cough", "tiredness", "loss of taste or smell", "loss of taste",
+                "loss of smell", "sore throat", "headache", "aches and pains", "diarrhoea", "discolouration of fingers",
+                "red or irritated eyes", "red eyes", "irritated eyes", "difficulty breathing", "loss of speech",
+                "chest pain" };
+        List<String> list0 = Arrays.asList(covid_Symptoms);
+
+        String[] pneumonia_Symptoms = { "difficulty breathing", "sweating and shivering", "rapid heartbeat",
+                "loss of appetite", "chest pain", "joint and muscle pain", "high temperature", "coughing up blood",
+                "chest pain", "fatigue", "wheezing", "headaches" };
+        List<String> list1 = Arrays.asList(pneumonia_Symptoms);
+
+        String[] flu_Symptoms = { "fever", "feverish/chills", "feverish", "chills", "cough", "sore throat",
+                "runny or stuffy nose", "muscle or body aches", "headaches", "fatigue", "vomiting", "diarrhea", };
+        List<String> list2 = Arrays.asList(flu_Symptoms);
+
+        // Different variables
+        String answer0, answer1;
+        int count = 0;
+
+        System.out.println("\tMedocles V2.0");
+        covid_Symptoms();
+        System.out.println("What symptoms are you showing?");
+
+        answer0 = input.nextLine();
+        answer0 = answer0.toLowerCase();
+
+        if (answer0.equals(null)) {
+            System.exit(1);
+        }
+
+        if (list0.contains(answer0)) {
+            do {
+                covid_Symptoms();
+                System.out.print("Do you show any other symptoms from the above (yes or no)?");
+                answer1 = input.nextLine();
+                answer1 = answer1.toLowerCase();
+                switch (answer1) {
+                case "yes":
+                    yes_answer(list0, answer0);
+                    break;
+                case "no":
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.print("Please answer with yes or no: ");
+                    break;
+                }
+            } while (list0.contains(answer0));
+        }
+    }
+}
