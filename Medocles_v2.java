2ae5f73b1a204bb7fff3c47bb2aab978ec1b28ce
Damocles <democleschannel28@gmail.com>
2021-11-21 13:11:05 +0100

    Medocles v2.0.4

--- Medocles_v2.java
+++ Medocles_v2.java
@@ -7,33 +7,34 @@ import java.util.stream.Collectors;
 public class Medocles_v2 {
     static void covid_Symptoms() {
         System.out.println("----------------------------------------------");
-        System.out.println("  Fever?");
-        System.out.println("  Cough?");
-        System.out.println("  Tiredness?");
-        System.out.println("  Loss of Taste or Smell?");
-        System.out.println("  Sore throat?");
-        System.out.println("  Headache?");
-        System.out.println("  Aches and pains?");
-        System.out.println("  Diarrhoea?");
-        System.out.println("  Discolouration of fingers?");
-        System.out.println("  Red or irritated eyes?");
-        // System.out.println(" All of them?");
+        System.out.println("1)  Fever?");
+        System.out.println("2)  Cough?");
+        System.out.println("3)  Tiredness?");
+        System.out.println("4)  Loss of Taste or Smell?");
+        System.out.println("5)  Sore throat?");
+        System.out.println("6)  Headache?");
+        System.out.println("7)  Aches and pains?");
+        System.out.println("8)  Diarrhoea?");
+        System.out.println("9)  Discolouration of fingers?");
+        System.out.println("10) Red or irritated eyes?");
+        System.out.println("    Type 10 for all of them");
         System.out.println("----------------------------------------------");
     }
 
     static void pneumonia_Symptoms() {
         System.out.println("----------------------------------------------");
-        System.out.println("  Difficulty breathing?");
-        System.out.println("  Sweating and/or shivering?");
-        System.out.println("  Rapid heartbeat?");
-        System.out.println("  Loss of appettite?");
-        System.out.println("  Chest pain?");
-        System.out.println("  Joint and/or muscle pain?");
-        System.out.println("  High temperature?");
-        System.out.println("  Coughing up blood?");
-        System.out.println("  Fatigue?");
-        System.out.println("  Wheezing?");
-        System.out.println("  Headaches?");
+        System.out.println("1)  Difficulty breathing?");
+        System.out.println("2)  Sweating and/or shivering?");
+        System.out.println("3)  Rapid heartbeat?");
+        System.out.println("4)  Loss of appettite?");
+        System.out.println("5)  Chest pain?");
+        System.out.println("6)  Joint and/or muscle pain?");
+        System.out.println("7)  High temperature?");
+        System.out.println("8)  Coughing up blood?");
+        System.out.println("9)  Fatigue?");
+        System.out.println("10) Wheezing?");
+        System.out.println("11) Headaches?");
+        System.out.println("    Type 11 for all of them");
         // System.out.println(" All of them?");
         System.out.println("  Maybe one of the symptoms above are more accurate to what you have?");
     }
@@ -41,16 +42,16 @@ public class Medocles_v2 {
     static void flu_Symptoms() {
         // TODO: Edit the symptoms for flu
         System.out.println("----------------------------------------------");
-        System.out.println("  Fever?");
-        System.out.println("  Feverish and/or Chills?");
-        System.out.println("  Cough?");
-        System.out.println("  Sore throat?");
-        System.out.println("  Runny or stuffy nose?");
-        System.out.println("  Muscle or body aches?");
-        System.out.println("  Headaches?");
-        System.out.println("  Vomiting?");
-        System.out.println("  Fatigue?");
-        System.out.println("  Diarrhoea?");
+        System.out.println("1)  Feverish and/or Chills?");
+        System.out.println("2)  Cough?");
+        System.out.println("3)  Sore throat?");
+        System.out.println("4)  Runny or stuffy nose?");
+        System.out.println("5)  Muscle or body aches?");
+        System.out.println("6)  Headaches?");
+        System.out.println("7)  Vomiting?");
+        System.out.println("8)  Fatigue?");
+        System.out.println("9)  Diarrhoea?");
+        System.out.println("    Type 9 for all of them");
         // System.out.println(" All of them?");
         System.out.println("  Maybe one of the symptoms above are more accurate to what you have?");
     }
@@ -59,7 +60,8 @@ public class Medocles_v2 {
         Scanner input = new Scanner(System.in);
 
         float chance = 1;
-        float size = list.size();
+        // float size = list.size();
+        float size = 10;
 
         ArrayList<String> answers = new ArrayList<String>();
         answers.add(before_answer);
@@ -69,31 +71,42 @@ public class Medocles_v2 {
 
         System.out.print("How many of the symptoms above are you showing? ");
         n = input.nextInt();
-        String[] a = new String[n];
-        System.out.println("Enter the symptoms that you show below.");
-        for (int i = 0; i <= n; i++) {
-            answer = input.nextLine();
-            answer = answer.toLowerCase();
-            answers.add(answer);
-        }
-        answers.remove(1);
-        if (!list.containsAll(answers)) {
-            System.out.println("The symptoms you have entered are not correct");
-            System.exit(0);
-        } else {
+        // String[] a = new String[n];
+        if (n == 10) {
+            answers.addAll(list);
             answers.replaceAll(String::toUpperCase);
             String result = answers.stream().map(x -> String.valueOf(x)).collect(Collectors.joining(" | ", "[ ", " ]"));
             System.out.println(result);
-            chance = ((n + 1) / size) * 100;
-            System.out.println("The chances of you containing Covid-19 based on the symptoms is: " + chance + "%");
+            System.out.println("The chance of you containing Covid-19 based on the symptoms is: 100%");
+
+        } else {
+            System.out.println("Enter the symptoms that you show below.");
+            for (int i = 0; i <= n; i++) {
+                answer = input.nextLine();
+                answer = answer.toLowerCase();
+                answers.add(answer);
+            }
+            answers.remove(1);
+            if (!list.containsAll(answers)) {
+                System.out.println("The symptoms you have entered are not correct");
+                System.exit(0);
+            } else {
+                answers.replaceAll(String::toUpperCase);
+                String result = answers.stream().map(x -> String.valueOf(x))
+                        .collect(Collectors.joining(" | ", "[ ", " ]"));
+                System.out.println(result);
+                chance = ((n + 1) / size) * 100;
+                String formated = String.format("%.02f", chance);
+                System.out.println("The chance of you containing Covid-19 based on the symptoms is: " + formated + "%");
+            }
         }
     }
 
     static void no_answer(List<String> list0, List<String> list1, String before_answer) {
         Scanner input = new Scanner(System.in);
         float chance = 1;
-        float size0 = list0.size(), size1 = list1.size();
-
+        // float size0 = list0.size(), size1 = list1.size();
+        float size0 = 11, size1 = 9;
         ArrayList<String> answers = new ArrayList<String>();
         answers.add(before_answer);
         // System.out.println(answers);
@@ -107,23 +120,34 @@ public class Medocles_v2 {
         if (answer1.equals("yes")) {
             System.out.print("How many of the symptoms above are you showing? ");
             n = input.nextInt();
-            System.out.println("Enter the symptoms that you show below.");
-            for (int i = 0; i <= n; i++) {
-                answer0 = input.nextLine();
-                answer0 = answer0.toLowerCase();
-                answers.add(answer0);
-            }
-            answers.remove(1);
-            if (!list0.containsAll(answers)) {
-                System.out.println("The symptoms you have entered are not correct");
-                System.exit(0);
-            } else {
+            if (n == 11) {
+                answers.addAll(list0);
                 answers.replaceAll(String::toUpperCase);
                 String result = answers.stream().map(x -> String.valueOf(x))
                         .collect(Collectors.joining(" | ", "[ ", " ]"));
                 System.out.println(result);
-                chance = ((n + 1) / size0) * 100;
-                System.out.println("The chances of you containing Pneumonia based on the symptoms is: " + chance + "%");
+                System.out.println("The chance of you containing Pneumonia based on the symptoms is: 100%");
+            } else {
+                System.out.println("Enter the symptoms that you show below.");
+                for (int i = 0; i <= n; i++) {
+                    answer0 = input.nextLine();
+                    answer0 = answer0.toLowerCase();
+                    answers.add(answer0);
+                }
+                answers.remove(1);
+                if (!list0.containsAll(answers)) {
+                    System.out.println("The symptoms you have entered are not correct");
+                    System.exit(0);
+                } else {
+                    answers.replaceAll(String::toUpperCase);
+                    String result = answers.stream().map(x -> String.valueOf(x))
+                            .collect(Collectors.joining(" | ", "[ ", " ]"));
+                    System.out.println(result);
+                    chance = ((n + 1) / size0) * 100;
+                    String formated = String.format("%.02f", chance);
+                    System.out.println(
+                            "The chance of you containing Pneumonia based on the symptoms is: " + formated + "%");
+                }
             }
         } else if (answer1.equals("no")) {
             flu_Symptoms();
@@ -133,23 +157,35 @@ public class Medocles_v2 {
             if (answer2.equals("yes")) {
                 System.out.print("How many of the symptoms above are you showing? ");
                 n = input.nextInt();
-                System.out.println("Enter the symptoms that you show below.");
-                for (int i = 0; i <= n; i++) {
-                    answer3 = input.nextLine();
-                    answer3 = answer3.toLowerCase();
-                    answers.add(answer3);
-                }
-                answers.remove(1);
-                if (!list1.containsAll(answers)) {
-                    System.out.println("The symptoms you have entered are not correct");
-                    System.exit(0);
-                } else {
+                if (n == 9) {
+                    answers.addAll(list0);
                     answers.replaceAll(String::toUpperCase);
                     String result = answers.stream().map(x -> String.valueOf(x))
                             .collect(Collectors.joining(" | ", "[ ", " ]"));
                     System.out.println(result);
-                    chance = ((n + 1) / size1) * 100;
-                    System.out.println("The chances of you containing Flu based on the symptoms is: " + chance + "%");
+                    System.out.println("The chance of you containing Flu based on the symptoms is: 100%");
+                } else {
+
+                    System.out.println("Enter the symptoms that you show below.");
+                    for (int i = 0; i <= n; i++) {
+                        answer3 = input.nextLine();
+                        answer3 = answer3.toLowerCase();
+                        answers.add(answer3);
+                    }
+                    answers.remove(1);
+                    if (!list1.containsAll(answers)) {
+                        System.out.println("The symptoms you have entered are not correct");
+                        System.exit(0);
+                    } else {
+                        answers.replaceAll(String::toUpperCase);
+                        String result = answers.stream().map(x -> String.valueOf(x))
+                                .collect(Collectors.joining(" | ", "[ ", " ]"));
+                        System.out.println(result);
+                        chance = ((n + 1) / size1) * 100;
+                        String formated = String.format("%.02f", chance);
+                        System.out.println(
+                                "The chances of you containing Flu based on the symptoms is: " + formated + "%");
+                    }
                 }
             } else {
                 System.exit(0);
@@ -173,11 +209,11 @@ public class Medocles_v2 {
                 "muscle pain", "high temperature", "coughing up blood", "fatigue", "wheezing", "headaches", "cough",
                 "tiredness", "loss of taste or smell", "loss of taste", "loss of smell", "sore throat", "headache",
                 "aches and pains", "diarrhoea", "discolouration of fingers", "red or irritated eyes", "red eyes",
-                "irritated eyes", "difficulty breathing", "loss of speech", "chest pain" };
+                "irritated eyes", "loss of speech", "loss of appettite" };
         List<String> list1 = Arrays.asList(pneumonia_Symptoms);
 
         String[] flu_Symptoms = { "fever", "feverish/chills", "feverish", "chills", "cough", "sore throat",
-                "runny or stuffy nose", "muscle or body aches", "headaches", "fatigue", "vomiting", "diarrhea", "cough",
+                "runny or stuffy nose", "muscle or body aches", "headaches", "fatigue", "vomiting", "diarrhea",
                 "tiredness", "loss of taste or smell", "loss of taste", "loss of smell", "sore throat", "headache",
                 "aches and pains", "diarrhoea", "discolouration of fingers", "red or irritated eyes", "red eyes",
                 "irritated eyes", "difficulty breathing", "loss of speech", "chest pain" };
@@ -187,7 +223,7 @@ public class Medocles_v2 {
         String answer0, answer1;
         // int count = 0;
 
-        System.out.println("\tMedocles V2.0.3");
+        System.out.println("\tMedocles V2.0.4");
         covid_Symptoms();
         System.out.println("What symptoms are you showing?");
 
